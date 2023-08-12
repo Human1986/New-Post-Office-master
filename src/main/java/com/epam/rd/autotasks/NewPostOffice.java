0efcd05a7d785b698f99017a00c06a17d020e112
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-08-12 09:42:58 +0200

    There are two test left to do.

--- src/main/java/com/epam/rd/autotasks/NewPostOffice.java
+++ src/main/java/com/epam/rd/autotasks/NewPostOffice.java
@@ -6,17 +6,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class NewPostOffice {
-    private final Collection<Box> listBox;
     private static final int COST_KILOGRAM = 5;
     private static final int COST_CUBIC_METER = 100;
     private static final double COEFFICIENT = 0.5;
+    private final Collection<Box> listBox;
 
     public NewPostOffice() {
         listBox = new ArrayList<>();
-    }
-
-    public Collection<Box> getListBox() {
-        return (Collection<Box>) ((ArrayList<Box>) listBox).clone();
     }
 
     static BigDecimal calculateCostOfBox(double weight, double volume, int value) {
@@ -28,18 +24,43 @@ public class NewPostOffice {
                 .add(BigDecimal.valueOf(COEFFICIENT * value), MathContext.DECIMAL64);
     }
 
+    public Collection<Box> getListBox() {
+        return (Collection<Box>) ((ArrayList<Box>) listBox).clone();
+    }
+
     // implements student
     public boolean addBox(String addresser, String recipient, double weight, double volume, int value) {
-        throw new UnsupportedOperationException();
+        checkedBox(addresser, recipient, weight, volume, value);
+
+        Box box = new Box(addresser, recipient, weight, volume);
+        BigDecimal cost = calculateCostOfBox(weight, volume, value);
+        box.setCost(cost);
+        listBox.add(box);
+        return true;
+    }
+
+    private void checkedBox(String addresser, String recipient, double weight, double volume, int value) {
+        if (value <= 0) throw new IllegalArgumentException();
+        if (addresser == null || recipient == null || recipient.isBlank() || addresser.isBlank())
+            throw new IllegalArgumentException();
+        if (volume < 0 || volume > 0.25 || volume == 0) throw new IllegalArgumentException();
+        if (weight < 0.5 || weight > 20.0) throw new IllegalArgumentException();
+        if (addresser.isEmpty() || recipient.isEmpty()) throw new IllegalArgumentException();
     }
 
     // implements student
     public Collection<Box> deliveryBoxToRecipient(String recipient) {
-        throw new UnsupportedOperationException();
+        listBox.removeIf(box -> box.getRecipient().equals(recipient));
+        return listBox;
     }
 
     public void declineCostOfBox(double percent) {
-        throw new UnsupportedOperationException();
+        for (Box box : listBox) {
+            BigDecimal percentDecline = new BigDecimal(percent);
+//            BigDecimal cost = NewPostOffice.calculateCostOfBox(box.getWeight(), box.getVolume(), box.getCost());
+            if (box.getCost() == null) box.setCost(box.getCost().add(percentDecline));
+        }
+
     }
 
 }
